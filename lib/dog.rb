339852bb15a15c544c606a13cd0a4e8a2bf78cb5
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-29 17:37:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 class Dog
- 
+
   @@all = []
- 
+
   attr_accessor :name
 
   def clear_all
     @@all.clear
   end
-    
- 
+
+
   def initialize(name)
     @name = name
     @@all << self
   end
- 
+
   def self.all
     @@all
   end
